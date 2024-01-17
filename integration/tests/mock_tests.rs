4a6375579193f8ee6629dad4ee161bf8d401f5ea
Vladimir Trifonov <trifonov.vp@gmail.com>
2024-01-17 16:47:57 +0200

    wip: fix

--- integration/tests/mock_tests.rs
+++ integration/tests/mock_tests.rs
@@ -1,24 +1,23 @@
 use ethers_core::types::H256;
 use integration::test_util::load_block_traces_for_test;
 use prover::{inner::Prover, utils::init_env_and_log, zkevm::circuit::SuperCircuit, ChunkTrace};
-use std::str::FromStr;
+// use std::str::FromStr;
 
-#[cfg(feature = "prove_verify")]
+// #[cfg(feature = "prove_verify")]
 #[test]
 fn test_mock_prove() {
     init_env_and_log("mock_tests");
 
-    let h256 = H256::from_str("3c8e0460c773f86d9170137735eebb0efdb8336362fe98ff0b4b82dd8dff274d").expect("Invalid hex string");
+    // let h256 = H256::from_str("0x3c8e0460c773f86d9170137735eebb0efdb8336362fe98ff0b4b82dd8dff274d").expect("Invalid hex string");
 
-    println!("{:?}", h256);
+    // println!("{:?}", h256);
 
     let block_traces = load_block_traces_for_test().1;
     Prover::<SuperCircuit>::mock_prove_target_circuit_batch(ChunkTrace {
         block_traces: block_traces.clone(),
+        last_applied_l1_block: Some(33),
+        prev_last_applied_l1_block: Some(33),
         ..Default::default()
-        // last_applied_l1_block: Some(32),
-        // prev_last_applied_l1_block: Some(33),
-        // l1_block_range_hash: Some(h256),
     })
     .unwrap();
 }
