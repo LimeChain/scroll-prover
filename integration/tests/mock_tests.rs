5dee37e8cbabcfe932d864fa303293e5a74126b7
Vladimir Trifonov <trifonov.vp@gmail.com>
2024-01-17 14:19:25 +0200

    wip: test with 1 tx L1BlockHashes

--- integration/tests/mock_tests.rs
+++ integration/tests/mock_tests.rs
@@ -1,5 +1,6 @@
 use integration::test_util::load_block_traces_for_test;
 use prover::{inner::Prover, utils::init_env_and_log, zkevm::circuit::SuperCircuit, ChunkTrace};
+use ethers_core::utils::{keccak256, H256};
 
 #[cfg(feature = "prove_verify")]
 #[test]
@@ -9,7 +10,11 @@ fn test_mock_prove() {
     let block_traces = load_block_traces_for_test().1;
     Prover::<SuperCircuit>::mock_prove_target_circuit_batch(ChunkTrace {
         block_traces: block_traces.clone(),
-        ..Default::default()
+        last_applied_l1_block: 32,
+        prev_last_applied_l1_block: 33,
+        l1_block_range_hash: Some(H256(
+            (keccak256(vec!["0xa5566b8827b9e1908989e9e6d3115950fd02deeedd291c6fba18cef10971f14f"])),
+        )),
     })
     .unwrap();
 }
