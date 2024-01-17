dd55afec2ba0a756c8f6491a02e7a81a2c660b99
Vladimir Trifonov <trifonov.vp@gmail.com>
2024-01-17 16:33:02 +0200

    wip: fix

--- integration/tests/mock_tests.rs
+++ integration/tests/mock_tests.rs
@@ -8,7 +8,7 @@ fn test_mock_prove() {
     init_env_and_log("mock_tests");
 
     let l1_block_range_hash = Some(H256("0x3c8e0460c773f86d9170137735eebb0efdb8336362fe98ff0b4b82dd8dff274d".as_bytes().try_into().expect("err")));
-    println!("l1_block_range_hash: {:?}", l1_block_range_hash)
+    println!("l1_block_range_hash: {:?}", l1_block_range_hash);
 
     let block_traces = load_block_traces_for_test().1;
     Prover::<SuperCircuit>::mock_prove_target_circuit_batch(ChunkTrace {
