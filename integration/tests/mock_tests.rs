f13ef17a4f76d912f4e0a75e4af1735bc2acc7af
Vladimir Trifonov <trifonov.vp@gmail.com>
2024-01-17 16:32:33 +0200

    wip: fix

--- integration/tests/mock_tests.rs
+++ integration/tests/mock_tests.rs
@@ -7,12 +7,15 @@ use prover::{inner::Prover, utils::init_env_and_log, zkevm::circuit::SuperCircui
 fn test_mock_prove() {
     init_env_and_log("mock_tests");
 
+    let l1_block_range_hash = Some(H256("0x3c8e0460c773f86d9170137735eebb0efdb8336362fe98ff0b4b82dd8dff274d".as_bytes().try_into().expect("err")));
+    println!("l1_block_range_hash: {:?}", l1_block_range_hash)
+
     let block_traces = load_block_traces_for_test().1;
     Prover::<SuperCircuit>::mock_prove_target_circuit_batch(ChunkTrace {
         block_traces: block_traces.clone(),
         last_applied_l1_block: Some(32),
         prev_last_applied_l1_block: Some(33),
-        l1_block_range_hash: Some(H256("0x3c8e0460c773f86d9170137735eebb0efdb8336362fe98ff0b4b82dd8dff274d".as_bytes().try_into().unwrap())),
+        l1_block_range_hash: l1_block_range_hash.clone(),
     })
     .unwrap();
 }
