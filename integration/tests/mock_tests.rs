d49f5e5f80fec13d085a3cc44b335371d0c57292
Vladimir Trifonov <trifonov.vp@gmail.com>
2024-02-01 20:16:48 +0200

    wip: test

--- integration/tests/mock_tests.rs
+++ integration/tests/mock_tests.rs
@@ -3,12 +3,14 @@ use prover::{inner::Prover, utils::init_env_and_log, zkevm::circuit::SuperCircui
 use std::str::FromStr;
 use ethers_core::abi::Hash;
 
-#[cfg(feature = "prove_verify")]
+// #[cfg(feature = "prove_verify")]
 #[test]
 fn test_mock_prove() {
     init_env_and_log("mock_tests");
 
     let block_traces = load_block_traces_for_test().1;
+    println!("Loaded chunk trace {:?}", block_traces.clone()[0].transactions[0]);
+    panic!("test");
     Prover::<SuperCircuit>::mock_prove_target_circuit_batch(ChunkTrace {
         block_traces: block_traces.clone(),
         last_applied_l1_block: Some(46),
